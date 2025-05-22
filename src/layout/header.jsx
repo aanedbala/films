507ddcf760e41bad2892f7f28911850333192090
Anastasiia <162581529+aanedbala@users.noreply.github.com>
2025-05-22 14:21:01 +0300

    Update header.jsx

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -2,7 +2,7 @@ function Header() {
     return <nav className="red darken-3">
         <div className="container">
             <div className="nav-wrapper">
-                <a href="!#"
+                <a href="https://github.com/aanedbala/films"
                 className="brand-logo">React Movies</a>
                 <ul id="nav-mobile" className="right
                 hide-on-med-and-down">
